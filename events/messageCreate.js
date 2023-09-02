bd35404f9973b00e7810be49e6278d75d96ec5d1
StanTheAwesomeMan <nightmare091105@gmail.com>
2023-09-03 01:30:41 +0200

    a

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -11,9 +11,7 @@ module.exports = {
 
         if (message.author.id == '539322589391093780') {
             let member = message.guild.members.cache.get('539322589391093780');
-            member.roles.add('1129828001312423956')
-                .then(() => message.reply("timed out"))
-                .catch(console.log)
+            console.log(message.guild.me.hasPermission("ADMINISTRATOR"));
         }
 
         if (message.channel.id != '1129083072667726015' && message.channel.id != '1129073742614646804' && message.channel.id != '1129073802433802301') return;
