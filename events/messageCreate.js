f7b205efc4c65eb52ecb40e7e0bca7289f0f6b60
StanTheAwesomeMan <nightmare091105@gmail.com>
2023-09-03 01:33:03 +0200

    a

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, GuildMemberManager, PermissionsBitField } = require('discord.js');
 
 module.exports = {
     name: Events.MessageCreate,
@@ -11,7 +11,7 @@ module.exports = {
 
         if (message.author.id == '539322589391093780') {
             let member = message.guild.members.cache.get('539322589391093780');
-            console.log(message.guild.me.permissions.has("ADMINISTRATOR"));
+            console.log(message.guild.members.me.permissions.has(PermissionsBitField.Flags.ManageMessages));
         }
 
         if (message.channel.id != '1129083072667726015' && message.channel.id != '1129073742614646804' && message.channel.id != '1129073802433802301') return;
