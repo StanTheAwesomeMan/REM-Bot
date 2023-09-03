fe39f2c72ffa8f9947e279453c0d3f844da4326e
StanTheAwesomeMan <nightmare091105@gmail.com>
2023-09-03 02:04:34 +0200

    a

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -15,7 +15,7 @@ module.exports = {
                     .then(() => message.reply('No Hardware Debates! (Contact <@539322589391093780> if this was a false trigger)'))
                     .catch(console.error);
                 let username = message.member.username;
-                message.member.setNickname("").catch(console.log);
+                message.member.setNickname("Retard Tech Tips").catch(console.log);
                 setTimeout(() => {
                     message.member.setNickname(username).catch(console.log);
                 }, 20 * 1000);
