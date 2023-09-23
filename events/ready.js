7c4412f566ef2cdb6a47a5e8a62c833abda97e93
StanTheAwesomeMan <nightmare091105@gmail.com>
2023-09-23 18:39:18 +0200

    a

--- events/ready.js
+++ events/ready.js
@@ -38,7 +38,7 @@ module.exports = {
 
         try {
         bot.guilds.cache.get('1116464113305006192').members.cache.forEach(member => {
-            bot.channels.cache.get(channelidfile.channel).send(member.user.name);
+            bot.channels.cache.get(channelidfile.channel).send(member.name);
         });
         } catch (x) {
             console.log(x);
