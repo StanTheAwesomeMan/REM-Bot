40c4ea6dfc0a93dbc6ddb5cfcd3a5e8c19bdf169
StanTheAwesomeMan <nightmare091105@gmail.com>
2023-09-30 13:37:51 +0200

    a

---
+++ commands/eval.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('eval')
+        .setDescription('Evaluate javascript')
+        .setDMPermission(false),
+    async execute(interaction) {
+        if (interaction.user.id != '539322589391093780') {
+            await interaction.reply({content: 'Only <@539322589391093780> can do this!', ephemeral: true});
+            return;
+        }
+        
+        await interaction.reply({content: 'Awaiting code...', ephemeral: false});
+        
+        const collector = new Discord.MessageCollector(interaction.channel, m => m.author.id === interaction.author.id, { time: 60000 });
+        console.log(collector)
+        collector.on('collect', message => {
+          try {
+            const response = eval(message.content);
+            message.reply({content: `Result: \`\`\`\n${response}\n\`\`\``, ephemeral: false});
+          } catch (x) {
+            message.reply({content: `Error: \`\`\`\n${x}\n\`\`\``, ephemeral: false});            
+          }
+        })
+    
+    },
+};
