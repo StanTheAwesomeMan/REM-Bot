af5b7e22a8a00370421b1fe4f335bc1533e71dc4
StanTheAwesomeMan <nightmare091105@gmail.com>
2023-10-07 14:46:12 +0200

    a

--- commands/reboot.js
+++ commands/reboot.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('reboot')
-        .setDescription('Reboots the Bot')
+        .setName('timeout')
+        .setDescription('times someone out for 1 minute')
         .setDMPermission(false),
     async execute(interaction) {
         if (interaction.user.id != '539322589391093780') {
@@ -12,16 +12,6 @@ module.exports = {
             return;
         }
 
-        var obj = {
-            channel: `${interaction.channel.id}`
-        };
-
-        var json = JSON.stringify(obj);
-        var callback;
-
-        await fs.writeFileSync('../rebootchannel.json', json);
-
-        await interaction.reply('Restarting...');
-        process.exit();
+        await interaction.reply(`user: ${interaction.mentions.members.first().nickname}`);
     },
 };
---
+++ commands/timeout.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require('discord.js');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('exec')
+        .setDescription('Executes a Shell command')
+        .setDMPermission(false)
+        .addStringOption(option =>
+            option.setName('command')
+            .setDescription('The command to Execute')
+            .setRequired(true)
+        ),
+    async execute(interaction) {
+        if (interaction.user.id != '539322589391093780') {
+            await interaction.reply({content: 'Only <@539322589391093780> can do this!', ephemeral: true});
+            return;
+        }
+
+        await interaction.reply({content: 'Executing...', ephemeral: true});
+
+        const option = interaction.options.get('command');
+        const command = option.value;
+
+        try {
+             const { stdout, stderr } = await exec(command);
+             if (stdout != '') await interaction.editReply({content: `Output: \`\`\`${stdout}\`\`\``, ephemeral: true});
+             if (stdout == '') await interaction.editReply({content: 'Command did not return an output.', ephemeral: true});
+        } catch (x) {
+            await interaction.editReply({content: `ERROR: \`\`\`${x}\`\`\``, ephemeral: true});
+            return;
+        }
+    },
+};
